dba0a8e060652008616170d4a4c916b56cb646a3
Fernando Espíndola <fer.esp@gmail.com>
2018-05-15 16:23:25 -0300

    add variable to start the lms or cms service

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-source /edx/app/edxapp/edxapp_env && python manage.py lms runserver 0.0.0.0:8001 --settings hedu
+source /edx/app/edxapp/edxapp_env && python manage.py "${EDXAPP_SERVICE}" runserver 0.0.0.0:8001 --settings hedu
