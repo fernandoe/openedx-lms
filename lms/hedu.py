444f6eb1431a8dd75699d11368df12460d410b7d
Fernando Espíndola <fer.esp@gmail.com>
2018-05-16 12:38:00 -0300

    copying information from the current docker environment

--- lms/hedu.py
+++ lms/hedu.py
@@ -54,63 +54,140 @@ CACHES = {
     }
 }
 
+# MODULESTORE = {
+#     "default": {
+#         "ENGINE": "xmodule.modulestore.mixed.MixedModuleStore",
+#         "OPTIONS": {
+#             "mappings": {},
+#             "stores": [
+#                 {
+#                     "NAME": "draft",
+#                     "DOC_STORE_CONFIG": {
+#                         "collection": "modulestore",
+#                         "db": "test",
+#                         "host": [
+#                             os.environ.get('MONGODB_MONGODB_SERVICE_HOST')
+#                         ],
+#                         "port": 27017
+#                     },
+#                     "ENGINE": "xmodule.modulestore.mongo.DraftMongoModuleStore",
+#                     "OPTIONS": {
+#                         "collection": "modulestore",
+#                         "db": "test",
+#                         "default_class": "xmodule.hidden_module.HiddenDescriptor",
+#                         "fs_root": "** OVERRIDDEN **",
+#                         "host": [
+#                             os.environ.get('MONGODB_MONGODB_SERVICE_HOST')
+#                         ],
+#                         "port": 27017,
+#                         "render_template": "edxmako.shortcuts.render_to_string"
+#                     }
+#                 },
+#                 {
+#                     "NAME": "xml",
+#                     "ENGINE": "xmodule.modulestore.xml.XMLModuleStore",
+#                     "OPTIONS": {
+#                         "data_dir": "** OVERRIDDEN **",
+#                         "default_class": "xmodule.hidden_module.HiddenDescriptor"
+#                     }
+#                 }
+#             ]
+#         }
+#     }
+# }
+#
+# CONTENTSTORE = {
+#     "DOC_STORE_CONFIG": {
+#         "collection": "modulestore",
+#         "db": "test",
+#         "host": [
+#             os.environ.get('MONGODB_MONGODB_SERVICE_HOST')
+#         ],
+#         "port": 27017
+#     },
+#     "ENGINE": "xmodule.contentstore.mongo.MongoContentStore",
+#     "OPTIONS": {
+#         "db": "test",
+#         "host": [
+#             os.environ.get('MONGODB_MONGODB_SERVICE_HOST')
+#         ],
+#         "port": 27017
+#     }
+# }
+
 MODULESTORE = {
-    "default": {
-        "ENGINE": "xmodule.modulestore.mixed.MixedModuleStore",
-        "OPTIONS": {
-            "mappings": {},
-            "stores": [
-                {
-                    "NAME": "draft",
-                    "DOC_STORE_CONFIG": {
-                        "collection": "modulestore",
-                        "db": "test",
-                        "host": [
-                            os.environ.get('MONGODB_MONGODB_SERVICE_HOST')
-                        ],
-                        "port": 27017
-                    },
-                    "ENGINE": "xmodule.modulestore.mongo.DraftMongoModuleStore",
-                    "OPTIONS": {
-                        "collection": "modulestore",
-                        "db": "test",
-                        "default_class": "xmodule.hidden_module.HiddenDescriptor",
-                        "fs_root": "** OVERRIDDEN **",
-                        "host": [
-                            os.environ.get('MONGODB_MONGODB_SERVICE_HOST')
-                        ],
-                        "port": 27017,
-                        "render_template": "edxmako.shortcuts.render_to_string"
-                    }
+    u'default': {
+        u'ENGINE': u'xmodule.modulestore.mixed.MixedModuleStore',
+        u'OPTIONS': {
+            u'stores': [{
+                u'ENGINE': u'xmodule.modulestore.split_mongo.split_draft.DraftVersioningModuleStore',
+                u'DOC_STORE_CONFIG': {
+                    u'replicaSet': u'',
+                    u'connectTimeoutMS': 2000,
+                    u'socketTimeoutMS': 3000,
+                    u'db': u'edxapp',
+                    u'collection': u'modulestore',
+                    u'ssl': False,
+                    u'host': [os.environ.get('MONGODB_MONGODB_SERVICE_HOST')],
+                    u'user': u'edxapp',
+                    u'read_preference': u'PRIMARY',
+                    u'password': u'password',
+                    u'port': 27017
                 },
-                {
-                    "NAME": "xml",
-                    "ENGINE": "xmodule.modulestore.xml.XMLModuleStore",
-                    "OPTIONS": {
-                        "data_dir": "** OVERRIDDEN **",
-                        "default_class": "xmodule.hidden_module.HiddenDescriptor"
-                    }
+                u'NAME': u'split',
+                u'OPTIONS': {
+                    u'fs_root': u'/edx/var/edxapp/data',
+                    u'render_template': u'edxmako.shortcuts.render_to_string',
+                    u'default_class': u'xmodule.hidden_module.HiddenDescriptor'
                 }
-            ]
+            }, {
+                u'ENGINE': u'xmodule.modulestore.mongo.DraftMongoModuleStore',
+                u'DOC_STORE_CONFIG': {
+                    u'replicaSet': u'',
+                    u'connectTimeoutMS': 2000,
+                    u'socketTimeoutMS': 3000,
+                    u'db': u'edxapp',
+                    u'collection': u'modulestore',
+                    u'ssl': False,
+                    u'host': [os.environ.get('MONGODB_MONGODB_SERVICE_HOST')],
+                    u'user': u'edxapp',
+                    u'read_preference': u'PRIMARY',
+                    u'password': u'password',
+                    u'port': 27017
+                },
+                u'NAME': u'draft',
+                u'OPTIONS': {
+                    u'fs_root': u'/edx/var/edxapp/data',
+                    u'render_template': u'edxmako.shortcuts.render_to_string',
+                    u'default_class': u'xmodule.hidden_module.HiddenDescriptor'
+                }
+            }
+            ], u'mappings': {}
         }
     }
 }
 
 CONTENTSTORE = {
-    "DOC_STORE_CONFIG": {
-        "collection": "modulestore",
-        "db": "test",
-        "host": [
-            os.environ.get('MONGODB_MONGODB_SERVICE_HOST')
-        ],
-        "port": 27017
+    u'ENGINE': u'xmodule.contentstore.mongo.MongoContentStore',
+    u'DOC_STORE_CONFIG': {
+        u'replicaSet': u'',
+        u'connectTimeoutMS': 2000,
+        u'socketTimeoutMS': 3000,
+        u'db': u'edxapp',
+        u'collection': u'modulestore',
+        u'ssl': False,
+        u'host': [os.environ.get('MONGODB_MONGODB_SERVICE_HOST')],
+        u'user': u'edxapp',
+        u'read_preference': u'PRIMARY',
+        u'password': u'password', u'port': 27017
     },
-    "ENGINE": "xmodule.contentstore.mongo.MongoContentStore",
-    "OPTIONS": {
-        "db": "test",
-        "host": [
-            os.environ.get('MONGODB_MONGODB_SERVICE_HOST')
-        ],
-        "port": 27017
+    u'ADDITIONAL_OPTIONS': {},
+    u'OPTIONS': {
+        u'db': u'edxapp',
+        u'ssl': False,
+        u'host': [os.environ.get('MONGODB_MONGODB_SERVICE_HOST')],
+        u'user': u'edxapp',
+        u'password': u'password',
+        u'port': 27017
     }
 }
